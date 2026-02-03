a90cc23d87d4b8ec208b5bc32e64277ac8e687e9
WangZhenghai <fisschl@163.com>
2026-02-03 14:08:12 +0800

    build.ps1: 自动创建 dist 目录（当不存在时）

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+# 自动创建 dist 目录（如果不存在）
+New-Item -ItemType Directory -Force -Path ./dist | Out-Null
+
 docker build -t static-server .
 $container = docker create static-server
 docker cp "${container}:/root/static-server" ./dist/static-server
